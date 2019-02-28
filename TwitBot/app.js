a587da69276b8e6ad44dff978c14da6d9da2280a
Jurena <31681551+Jurena@users.noreply.github.com>
2019-02-28 15:18:57 -0500

    Fixed Bug
    The bug mentioned earlier is now fixed, the bot now works as intended.

--- TwitBot/app.js
+++ TwitBot/app.js
@@ -8,15 +8,14 @@ setInterval(retweet, 1000*20);
 
 function retweet(){
 	var tweets = [];
-	T.get('search/tweets', { q: 'Michael Cohen since:2019-01-25', count:10},
+	T.get('search/tweets', { q: 'Cohen since:2019-01-25', count:10},
 		function(err, data, response){
 			for (var i=0; i<(data.statuses).length; i++){
 				tweets.push(data.statuses[i].text);
+				var randomInd=Math.floor(Math.random()*tweets.length);
+				T.post('statuses/update', {status: tweets[randomInd] }, function(err, data, response){
+					console.log(`posted ${tweets[randomInd]}`);
+				})
 			}
 		})
-	console.log(tweets);
-	var randomInd=Math.floor(Math.random()*tweets.length);
-	T.post('statuses/update', {status: tweets[randomInd] }, function(err, data, response){
-		console.log(`posted ${tweets[randomInd]}`);
-	})
 }
