e0014e2b1586eacf825dea733c794d490d1f5a5f
Jurena <31681551+Jurena@users.noreply.github.com>
2019-02-28 14:02:16 -0500

    Create app.js
    This is a rudimentary Twitter Bot that is supposed to pull 10 tweets, any of which can fall between Jan 25th 2019 and the current date, that contain the name "Michael Cohen" within them, push the text of those tweets onto an array and then retweet said tweets randomly from that array. Currently I am running into a bug that prevents the bot from pulling tweets, so the bot is currently tweeting 'undefined', meaning it's not tweeting anything.

---
+++ TwitBot/app.js
@@ -0,0 +1,22 @@
+console.log('The Bot is Starting');
+
+const Twit = require('twit'); 
+
+const config = require('./config'); 
+var T = new Twit(config); 
+setInterval(retweet, 1000*20);
+
+function retweet(){
+	var tweets = [];
+	T.get('search/tweets', { q: 'Michael Cohen since:2019-01-25', count:10},
+		function(err, data, response){
+			for (var i=0; i<(data.statuses).length; i++){
+				tweets.push(data.statuses[i].text);
+			}
+		})
+	console.log(tweets);
+	var randomInd=Math.floor(Math.random()*tweets.length);
+	T.post('statuses/update', {status: tweets[randomInd] }, function(err, data, response){
+		console.log(`posted ${tweets[randomInd]}`);
+	})
+}
